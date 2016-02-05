549904ce6fafd079f24530548ea6465fda1ce024
julenl <julenl@gmail.com>
2016-02-05 19:51:16 +0100

    Ubuntu/Liberty preliminary version

---
+++ Ubuntu-Liberty/bomsi-iso.sh
@@ -0,0 +1,101 @@
+#! /bin/bash
+#set -e  #for debuging
+#set -x 
+export BOMSI_ISO_RELEASE="Liberty"
+export BOMSI_ISO_VERSION="0.1"
+export BOMSI_ISO_OPSYS="Ubuntu"
+## 
+## This file is the main driver of BOMSI, the Bash OpenStack Multinode Scripted Installer. 
+## It calls the following libraries:
+## - bomsi_vars: main variables, such as IPs, passwords, ...
+## - bomsi_lib_conf: library containing functions to install and configure OpenStack 
+## - bomsi_lib_vm: library containing functions to handdle local (test) virtual machines and network settings
+## - bomsi_susti.py: python (OK this is not BASH :P) script for handeling the ".ini" configuration files
+## - gather_packages_os.sh: function for gathering all needed packages for off-line (or just faster) install
+## - get_args.sh: command line argument parser for bomsi-iso.sh (i.e. ./bomsi-iso.sh -h)
+## - gen_ks.sh: library for generating the kickstart and installation files for all machines
+## - bomsi_os_test: library for testing and troubleshooting each OpenStack machine
+## 
+##
+
+
+
+export THISD=${PWD}
+
+[ -f $THISD/lib/bomsi_vars ] && . $THISD/lib/bomsi_vars
+
+## Parse command line options
+CMDL_VARS="$@"
+source get_args.sh
+
+
+
+## Ensure that all variables are loaded/defined
+
+export BOOT_TIMEOUT="100"   # time until the iso bootloaders loads the default option (seconds x10)
+[ -z ${ROOT_PASSWORD+x} ] && export ROOT_PASSWORD="1234" #Root/User password for the server
+export OUT_DIR="/tmp/custom_iso" # Dir. where the original ISO file is uncompressed and processed
+export OUT_ISO_DIR="$HOME" # Dir. where the new ISO file is generated
+export PATH_TO_ISO="$HOME/ISOS/ubuntu-15.10-server-amd64.iso"
+[ -z ${OUT_ISO_NAME+x} ] && export OUT_ISO_NAME="Ubuntu_custom.iso"
+[ ! -z ${HD+x} ] && export HD="vda" # name of the disk where the system will be installed
+[ ! -z ${VCPUS+x} ] && export VCPUS=2 # No. of virtual CPUs for each test VM
+[ ! -z ${VRAM+x} ]  && export VRAM=4092 # Mb of RAM for test VMs i.e. 8192
+
+export NETMASK="255.255.255.0"
+
+export DS=5 # root virtual HD Size of 5 Gb. Minimum 4 Gb, recommended 8
+export DS_B=2 # virtual HD Size of 4 Gb for block storage
+export DS_O=2 # virtual HD Size of 2 Gb for object storage
+
+
+## If the original ISO is not present, download it into the $PATH_TO_ISO directory
+which curl > /dev/null || sudo apt-get -y install curl >> /tmp/installed_by_bomsi.log
+if [ ! -f $PATH_TO_ISO ]; then
+  mkdir -p ${PATH_TO_ISO%/*} > /dev/null 
+  #curl -o $PATH_TO_ISO http://cdimage.ubuntu.com/lubuntu/releases/15.10/release/lubuntu-15.10-desktop-amd64.iso
+  curl -o $PATH_TO_ISO http://de.releases.ubuntu.com/15.10/ubuntu-15.10-server-amd64.iso
+fi
+
+
+## Load and execute the function that downloads and rebuilds the custom ISO file
+. lib/iso_kickstart
+iso_kickstart
+
+
+## The ISO is already created. Now, if $USB_DEV was defined it will install the ISO on that device
+## if the $INSTALL_VM variable was defined at some point, it will load and run the start_iso_vm,
+## which installs the ISO file into a Virtual Machine with virsh
+
+
+## Copy (install) the newly created ISO into a pendrive
+
+if [ ! -z ${USB_DEV+x} ]; then
+  echo ">> clean the content of the pendrive"
+  sudo dd if=/dev/zero of=$USB_DEV bs=512 count=1
+  #echo -e "o\nn\np\n1\n\n\na\n1\nw" | sudo fdisk $USB_DEV
+  echo " "
+  echo ">> COPY THE ISO FILE TO THE PENDRIVE IN $USB_DEV "
+  echo " "
+  sudo dd if=$OUT_ISO_DIR/$OUT_ISO_NAME of=$USB_DEV bs=512 
+  echo ">> Testing USB device"
+  sudo qemu-system-x86_64 -enable-kvm -m 1024 -hda $USB_DEV
+  unset INSTALL_VM
+fi
+
+
+
+
+## Load the ISO into a Virtual Machine with qemu
+
+if [ -z ${INSTALL_VM+x} ]; then 
+  echo ">> Not starting VM."; 
+else 
+  . $THISD/lib/start_iso_vm 
+  start_iso_vm 
+fi
+
+
+
+
+
---
+++ Ubuntu-Liberty/get_args.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+
+HELP_TEXT="""
+bomsi-iso version $BOMSI_ISO_VERSION for OpenStack release $BOMSI_ISO_RELEASE.
+-i, --install_vm              Install a virtual machine with the created iso image
+-n, --name=VM_NAME            Name for the quemu virtual machine being created.
+-r, --root_password=Password  Root password for the kickstarted machine
+-u, --create_usb=/dev/sdb     Create a bootable pendrive on the give device with the new ISO. disables VM
+-g, --gatewayln               Last number of Gateway IP in management network (10.0.0.1 --> 1)
+-d, --dnsln                   Last number of DNS IP in management network (10.0.0.1 --> 1)
+-m, --netmask                 Netmask (def. 255.255.255.0)
+--use_internet                Allow downloading packages from internet (def. NO)
+--no_firstrun                 Do not execute the init.d script after reboot
+--virt_nic_man=\"network=management\"
+                              The name of the management network (or bridge) for VMs (i.e. \"bridge=br0\")
+-v, --version                 Print version and exit
+-h, --help                    Print this help and exit
+
+Default configurations are provided for any name (-n) containing controller, compute1, compute2, compute3, network, block, object or clean. Clean kickstarts a clean CentOS machine, i.e. for dowloading the packages with gather_packages for a full off-line installation.
+
+Examples:
+Create an ISO image with a boot menu which allows to select which machine is going to be installed:
+./bomsi-iso.sh
+
+Create the previous ISO image and test it on a virtual machine called \"1.controller\":
+./bomsi-iso.sh -n=1.controller
+
+Create an ISO for installing the first compute node and burn it onto a bootable pendrive:
+(assuming the pendrive is inserted on the system and that it is on /dev/sdb)
+(this also starts a (diskless) qemu test machine, to test that the pendrive works fine)
+./bomsi-iso.sh -n=compute1 -u=/dev/sdb
+
+"""
+
+
+for i in "$@"
+do
+  case $i in
+      -i|--install_vm)
+        export INSTALL_VM=' '
+        shift # next value in @
+      ;;
+      -n=*|--vm_name=*)
+        export VM_NAME="${i#*=}"
+        export INSTALL_VM=' '
+        shift # past argument=value
+      ;;
+      -r=*|--root_password=*)
+        #export ROOT_PASSWORD="${i#*=}"
+        sustivar "ROOT_PASSWORD=${i#*=}"
+        shift 
+      ;;
+      -o=*|--output_iso_name=*)
+        export OUT_ISO_NAME="${i#*=}"
+        shift 
+      ;;
+      --no_firstrun)
+        export NO_FIRSTRUN=""
+        shift 
+      ;;
+      --use_internet)
+        export INSTALL_FROM_INTERNET="YES"
+        shift 
+      ;;
+      --virt_nic_man=*)
+        export VIRT_NIC_MAN="${i#*=}"
+        shift 
+      ;;
+      -u=*|--create_usb=*)
+        export USB_DEV="${i#*=}"
+        shift 
+      ;;
+      -g=*|--gatewayln=*)
+        sustivar "GATEWAY_LN=${i#*=}"
+        shift 
+      ;;
+      -d=*|--dnsln=*)
+        sustivar "DNS_LN=${i#*=}"
+        shift 
+      ;;
+      -v|--version)
+        echo "$BASH_ARGV version $VERSION"
+        exit
+      ;;
+      -h|--help|*)
+        echo "$HELP_TEXT"
+        exit
+      ;;
+      *)
+              # unkonwn options 
+      ;;
+  esac
+done
+
